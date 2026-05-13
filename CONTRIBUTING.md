61e287115b495e03273ea4f2bd48987806efe49b
paulrm <962154+paulrm@users.noreply.github.com>
2026-05-13 17:58:29 +0000

    Customise country-template through CI

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -57,7 +57,7 @@ Examples:
 
 ### Changelog
 
-openfisca-country_template changes must be understood by users who don't
+openfisca-ar changes must be understood by users who don't
 necessarily work on the code. The Changelog must therefore be as explicit as
 possible.
 
--- openfisca_ar/__init__.py
+++ openfisca_ar/__init__.py
@@ -14,8 +14,8 @@ import os
 
 from openfisca_core.taxbenefitsystems import TaxBenefitSystem
 
-from openfisca_country_template import entities
-from openfisca_country_template.situation_examples import couple
+from openfisca_ar import entities
+from openfisca_ar.situation_examples import couple
 
 COUNTRY_DIR = os.path.dirname(os.path.abspath(__file__))
 
